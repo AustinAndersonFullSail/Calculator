05fee409da43c4563bff39e81359f3456aea30cb
Austin Anderson <ATAnderson@student.fullsail.edu>
2022-07-18 11:59:23 -0600

    Processor Header file complete

---
+++ Calculator/CalculatorProcessor.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <string>
+
+class CalculatorProcessor
+{
+
+private:
+	static CalculatorProcessor* _processor;
+	int _baseNumber;
+	CalculatorProcessor() {}
+public:
+	static CalculatorProcessor* GetInstance();
+	void SetBaseNumber(int number);
+
+	CalculatorProcessor(CalculatorProcessor& copy) = delete;
+	void operator=(const CalculatorProcessor& assignment) = delete;
+
+	std::string GetDecimal();
+	std::string GetHexadecimal();
+	std::string GetBinary();
+};
+
