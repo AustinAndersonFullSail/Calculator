08376c906d81307df3094ff55a667d0c8d93afa0
Austin Anderson <ATAnderson@student.fullsail.edu>
2022-07-21 13:34:16 -0600

    Revert "Button factory tests complete (not tested)"
    This reverts commit 4301ddc789c3afec595ee52da0a068110540d9b7.

--- CalculatorTests/ButtonFactoryTests.cpp
+++ CalculatorTests/ButtonFactoryTests.cpp
@@ -14,64 +14,14 @@ namespace CalculatorTests
 		ButtonFactory buttonFactory;
 		wxFrame* parent = new wxFrame(nullptr, wxID_ANY, "TempFrame", wxPoint(100, 100), wxSize(517, 640));
 
-		TEST_METHOD(CheckClearID) {
+		TEST_METHOD(CheckClearButtonID) {
 			wxButton* clear = buttonFactory.CreateButtonClear(parent);
 			wxWindowID one = 1;
 			Assert::AreEqual(clear->GetId(), one);
 		}
 
-		TEST_METHOD(CheckClearLabel) {
-			wxButton* clear = buttonFactory.CreateButtonClear(parent);
-			wxString c = "C";
-			Assert::AreEqual(clear->GetLabel(), c);
-		}
+		
 
-		TEST_METHOD(CheckClearNotID) {
-			wxButton* clear = buttonFactory.CreateButtonClear(parent);
-			wxWindowID two = 2;
-			Assert::AreNotEqual(clear->GetId(), two);
-		}
-
-		TEST_METHOD(CheckClearNotLabel) {
-			wxButton* clear = buttonFactory.CreateButtonClear(parent);
-			wxString one = "1";
-			Assert::AreNotEqual(clear->GetLabel(), one);
-		}
-
-		TEST_METHOD(CheckOneID) {
-			wxButton* one = buttonFactory.CreateButton1(parent);
-			wxWindowID two = 2;
-			Assert::AreEqual(one->GetId(), two);
-		}
-
-		TEST_METHOD(CheckOneNotID) {
-			wxButton* one = buttonFactory.CreateButton1(parent);
-			wxWindowID three = 3;
-			Assert::AreNotEqual(one->GetId(), three);
-		}
-
-		TEST_METHOD(CheckOneLabel) {
-			wxButton* one = buttonFactory.CreateButton1(parent);
-			wxString oneLabel = "1";
-			Assert::AreEqual(one->GetLabel(), oneLabel);
-		}
-
-		TEST_METHOD(CheckOneNotLabel) {
-			wxButton* one = buttonFactory.CreateButton1(parent);
-			wxString two = "2";
-			Assert::AreNotEqual(one->GetLabel(), two);
-		}
-
-		TEST_METHOD(CheckTextBoxID) {
-			wxTextCtrl* textBox = buttonFactory.CreateTextBox(parent);
-			wxWindowID zero = 0;
-			Assert::AreEqual(textBox->GetId(), zero);
-		}
-
-		TEST_METHOD(CheckTextBoxNotID) {
-			wxTextCtrl* textBox = buttonFactory.CreateTextBox(parent);
-			wxWindowID one = 1;
-			Assert::AreNotEqual(textBox->GetId(), one);
-		}
+		
 	};
 }
